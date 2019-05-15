3df5cbd4a3ac920daf7868dfd7d7d107612101a9
Richard Neumann <r.neumann@homeinfo.de>
2019-05-15 15:57:29 +0200

    Refac.

--- hidslcfg/globals.py
+++ hidslcfg/globals.py
@@ -11,4 +11,4 @@ __all__ = ['APPLICATION_SERVICE', 'DIGSIG_DATA_DIR', 'LOGGER', 'LOG_FORMAT']
 APPLICATION_SERVICE = 'application.service'
 DIGSIG_DATA_DIR = Path('/usr/share/digsig')
 LOG_FORMAT = '[%(levelname)s] %(name)s: %(message)s'
-LOGGER = getLogger(argv[0])
+LOGGER = getLogger(Path(argv[0]).name)
