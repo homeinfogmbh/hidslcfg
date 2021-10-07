ba0e43916c671ab86e654fa672327b12fdb93e7a
Richard Neumann <mail@richard-neumann.de>
2021-10-07 10:15:20 +0200

    Remove export of CLI functions

--- hidslcfg/__init__.py
+++ hidslcfg/__init__.py
@@ -1,6 +1,1 @@
 """HOMEINFO Digital Signage Linux configurator."""
-
-from hidslcfg.cli import hidslcfg, hidslreset
-
-
-__all__ = ['hidslcfg', 'hidslreset']
--- hidslcfg/cli/__init__.py
+++ hidslcfg/cli/__init__.py
@@ -1,7 +1,1 @@
 """Command line interfaces."""
-
-from hidslcfg.cli.hidslcfg import run as hidslcfg
-from hidslcfg.cli.hidslreset import run as hidslreset
-
-
-__all__ = ['hidslcfg', 'hidslreset']
