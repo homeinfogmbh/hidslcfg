d5c1ccbda184145b2493d60706989c629045699d
Richard Neumann <r.neumann@homeinfo.de>
2022-04-12 16:40:53 +0200

    Clean up OpenVPN

--- hidslcfg/openvpn/common.py
+++ hidslcfg/openvpn/common.py
@@ -4,11 +4,9 @@ from ipaddress import IPv4Address
 from pathlib import Path
 
 
-__all__ = ['CLIENT_DIR', 'DEFAULT_SERVICE', 'GROUP', 'OWNER', 'SERVER']
+__all__ = ['CLIENT_DIR', 'DEFAULT_SERVICE', 'SERVER']
 
 
 CLIENT_DIR = Path('/etc/openvpn/client')
 DEFAULT_SERVICE = 'openvpn-client@terminals.service'
-GROUP = 'network'
-OWNER = 'openvpn'
 SERVER = IPv4Address('10.8.0.1')
