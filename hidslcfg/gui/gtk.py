41eb90146a2e55569fe189d2a232b012bc9e37f5
Richard Neumann <r.neumann@homeinfo.de>
2022-06-21 14:38:07 +0200

    Remove obsolete Gdk

--- hidslcfg/gui/gtk.py
+++ hidslcfg/gui/gtk.py
@@ -2,8 +2,7 @@
 
 from gi import require_version
 require_version('Gtk', '3.0')
-require_version('Gdk', '3.0')
-from gi.repository import Gdk, Gtk
+from gi.repository import Gtk
 
 
-__all__ = ['Gdk', 'Gtk']
+__all__ = ['Gtk']
