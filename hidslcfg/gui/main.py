cb7cb8d438c20320e988d549f6a90fe89155c39f
Richard Neumann <r.neumann@homeinfo.de>
2022-06-21 15:12:08 +0200

    Add error message

--- hidslcfg/gui/main.py
+++ hidslcfg/gui/main.py
@@ -1,6 +1,7 @@
 """Main GUI application."""
 
 from os import geteuid
+from sys import stderr
 
 from hidslcfg.gui.gtk import Gtk
 from hidslcfg.gui.login import LoginForm
@@ -10,6 +11,7 @@ def main() -> None:
     """Starts the GUI."""
 
     if geteuid() != 0:
+        print('This program required root privileges.', file=stderr)
         raise SystemExit(1)
 
     login_form = LoginForm()
