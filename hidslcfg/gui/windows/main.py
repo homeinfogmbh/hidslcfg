6e277a81b571a37988fabb9d068136c0dd0f9c2b
Richard Neumann <r.neumann@homeinfo.de>
2022-10-12 15:27:27 +0200

    Outsource ping tab

--- hidslcfg/gui/windows/main.py
+++ hidslcfg/gui/windows/main.py
@@ -1,22 +1,20 @@
 """Login window logic."""
 
 from logging import getLogger
-from subprocess import CalledProcessError
-from threading import Thread
 
 from hidslcfg.api import Client
 from hidslcfg.common import HIDSL_DEBUG
 from hidslcfg.gui.builder_window import BuilderWindow
 from hidslcfg.gui.gtk import Gtk
 from hidslcfg.gui.windows.login_tab import LoginTab
+from hidslcfg.gui.windows.ping_tab import PingTab
 from hidslcfg.gui.windows.wifi_tab import WifiTab
-from hidslcfg.system import ping, reboot
+from hidslcfg.system import reboot
 
 
 __all__ = ['MainWindow']
 
 
-DEFAULT_HOST = 'wireguard.homeinfo.de'
 LOGGER = getLogger(__file__)
 
 
@@ -27,9 +25,6 @@ class MainWindow(BuilderWindow, file='main.glade'):
         """Create the login form."""
         super().__init__('main', primary=self.build('tab_system_config'))
         self.client = client
-        self.logged_in = False
-        self.ping_successful: bool | None = None
-        self.reboot_response = None
 
         # Login tab
         self.login_tab = LoginTab(self)
@@ -38,64 +33,11 @@ class MainWindow(BuilderWindow, file='main.glade'):
         self.wifi_tab = WifiTab(self)
 
         # Ping tab
-        self.ping_hostname: Gtk.ComboBoxText = self.build('ping_hostname')
-        self.ping_hostname.connect("changed", self.on_ping_hostname_change)
-        self.ping_spinner: Gtk.Spinner = self.build('ping_spinner')
-        self.ping_host: Gtk.Button = self.build('ping_host')
-        self.ping_host_label: str = self.ping_host.get_label()
-        self.ping_host.connect('activate', self.on_ping_host)
-        self.ping_host.connect('clicked', self.on_ping_host)
-        self.ping_result: Gtk.Image = self.build('ping_result')
-        self.new_signal('ping-host-completed', self.on_ping_completed)
+        self.ping_tab = PingTab(self)
 
         self.btn_quit: Gtk.Button = self.build('quit')
         self.btn_quit.connect('activate', self.on_quit)
         self.btn_quit.connect('clicked', self.on_quit)
-
-    def ping_thread(self) -> None:
-        """Ping the host."""
-        try:
-            ping(self.ping_hostname.get_active_id())
-        except CalledProcessError:
-            self.ping_successful = False
-        else:
-            self.ping_successful = True
-
-        self.window.emit('ping-host-completed', None)
-
-    def on_ping_hostname_change(self, *_) -> None:
-        """Ping the set host."""
-        self.ping_result.set_from_icon_name(
-            'face-plain-symbolic',
-            Gtk.IconSize.LARGE_TOOLBAR
-        )
-
-    def on_ping_host(self, *args) -> None:
-        """Ping the set host."""
-        self.ping_host.set_property('sensitive', False)
-        self.ping_hostname.set_property('sensitive', False)
-        self.on_ping_hostname_change(*args)
-        self.ping_host.set_label('')
-        self.ping_spinner.start()
-        Thread(daemon=True, target=self.ping_thread).start()
-
-    def on_ping_completed(self, *_) -> None:
-        """Sets the ping result."""
-        self.ping_spinner.stop()
-        self.ping_host.set_label(self.ping_host_label)
-        self.ping_hostname.set_property('sensitive', True)
-        self.ping_host.set_property('sensitive', True)
-
-        if self.ping_successful:
-            self.ping_result.set_from_icon_name(
-                'face-smirk-symbolic',
-                Gtk.IconSize.LARGE_TOOLBAR
-            )
-        else:
-            self.ping_result.set_from_icon_name(
-                'face-sad-symbolic',
-                Gtk.IconSize.LARGE_TOOLBAR
-            )
 
     def on_quit(self, *_) -> None:
         """Handles the quit button."""
---
+++ hidslcfg/gui/windows/ping_tab.py
@@ -0,0 +1,74 @@
+"""Login window logic."""
+
+from subprocess import CalledProcessError
+from threading import Thread
+
+from hidslcfg.gui.builder_window import BuilderWindow
+from hidslcfg.gui.sub_element import SubElement
+from hidslcfg.gui.gtk import Gtk
+from hidslcfg.system import ping
+
+
+__all__ = ['PingTab']
+
+
+class PingTab(SubElement):
+    """Ping test tab."""
+
+    def __init__(self, window: BuilderWindow):
+        super().__init__(window)
+        self.ping_successful: bool | None = None
+        self.ping_hostname: Gtk.ComboBoxText = self.build('ping_hostname')
+        self.ping_hostname.connect("changed", self.on_ping_hostname_change)
+        self.ping_spinner: Gtk.Spinner = self.build('ping_spinner')
+        self.ping_host: Gtk.Button = self.build('ping_host')
+        self.ping_host_label: str = self.ping_host.get_label()
+        self.ping_host.connect('activate', self.on_ping_host)
+        self.ping_host.connect('clicked', self.on_ping_host)
+        self.ping_result: Gtk.Image = self.build('ping_result')
+        self.new_signal('ping-host-completed', self.on_ping_completed)
+
+    def on_ping_hostname_change(self, *_) -> None:
+        """Ping the set host."""
+        self.ping_result.set_from_icon_name(
+            'face-plain-symbolic',
+            Gtk.IconSize.LARGE_TOOLBAR
+        )
+
+    def on_ping_host(self, *args) -> None:
+        """Ping the set host."""
+        self.ping_host.set_property('sensitive', False)
+        self.ping_hostname.set_property('sensitive', False)
+        self.on_ping_hostname_change(*args)
+        self.ping_host.set_label('')
+        self.ping_spinner.start()
+        Thread(daemon=True, target=self.ping_thread).start()
+
+    def ping_thread(self) -> None:
+        """Ping the host."""
+        try:
+            ping(self.ping_hostname.get_active_id())
+        except CalledProcessError:
+            self.ping_successful = False
+        else:
+            self.ping_successful = True
+
+        self.window.emit('ping-host-completed', None)
+
+    def on_ping_completed(self, *_) -> None:
+        """Set the ping result."""
+        self.ping_spinner.stop()
+        self.ping_host.set_label(self.ping_host_label)
+        self.ping_hostname.set_property('sensitive', True)
+        self.ping_host.set_property('sensitive', True)
+
+        if self.ping_successful:
+            self.ping_result.set_from_icon_name(
+                'face-smirk-symbolic',
+                Gtk.IconSize.LARGE_TOOLBAR
+            )
+        else:
+            self.ping_result.set_from_icon_name(
+                'face-sad-symbolic',
+                Gtk.IconSize.LARGE_TOOLBAR
+            )
