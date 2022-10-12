74e4c74af47b1b4cbc6ff012ded3cc94558f4583
Richard Neumann <r.neumann@homeinfo.de>
2022-10-12 13:32:29 +0200

    Simplify SubElement

--- hidslcfg/gui/sub_element.py
+++ hidslcfg/gui/sub_element.py
@@ -3,7 +3,6 @@
 from __future__ import annotations
 
 from hidslcfg.gui.builder_window import BuilderWindow
-from hidslcfg.gui.gtk import Gtk
 
 
 __all__ = ['SubElement']
@@ -12,11 +11,8 @@ __all__ = ['SubElement']
 class SubElement:
     """Window sub-element."""
 
-    def __init__(self, window: Gtk.Window, builder: Gtk.Builder):
-        self.window: Gtk.Window = window
-        self.builder: Gtk.Builder = builder
+    def __init__(self, window: BuilderWindow):
+        self.window: BuilderWindow = window
 
-    @classmethod
-    def from_window(cls, window: BuilderWindow) -> SubElement:
-        """Create a sub-element from the main window."""
-        return cls(window.window, window.builder)
+    def __getattr__(self, item):
+        return getattr(self.window, item)
